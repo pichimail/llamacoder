5f6c716d3711585b82113551ea47220921941e0e
pichimail <pichimail24@gmail.com>
2026-06-30 12:58:24 +0530

    fix: add 'icon' size variant to InputGroupButton
    - Fixes TypeScript error where PromptInputButton and PromptInputSubmit
      defaulted to size="icon" which was not declared in inputGroupButtonVariants
    - Resolves Vercel build failure during type checking step

--- components/ui/input-group.tsx
+++ components/ui/input-group.tsx
@@ -89,6 +89,7 @@ const inputGroupButtonVariants = cva(
         "icon-xs":
           "size-6 rounded-[calc(var(--radius)-5px)] p-0 has-[>svg]:p-0",
         "icon-sm": "size-8 p-0 has-[>svg]:p-0",
+        icon: "size-8 p-0 has-[>svg]:p-0",
       },
     },
     defaultVariants: {
